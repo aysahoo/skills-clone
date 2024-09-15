27ceab8a25daee82847d15d1a5f2e0d110dab4c9
Ayush Ranjan Sahoo <77970726+aysahoo@users.noreply.github.com>
2024-09-15 22:58:10 +0530

    Case_sensitivity

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Timeline from '../components/Timeline'
 import Badges from '../components/Badges'
